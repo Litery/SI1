fb7608c409c09eca6ddf3515f952b0a432b930b2
Szon <s.litera@gmail.com>
2016-12-02 12:31:18 +0100

    Implement teaching in Network class

--- mlp.py
+++ mlp.py
@@ -10,7 +10,7 @@ class Neuron:
         self.__inputs = kwargs.get('inputs', None)
         self.last_output = None
         self.last_delta = None
-        self.bias = kwargs.get('bias', 4)
+        self.bias = kwargs.get('bias', 2)
         self.learn_rate = kwargs.get('learn_rate', 1)
         if self.weights is None:
             self.weights = []
@@ -76,7 +76,7 @@ class Neuron:
         self.delta_weights = [dw + nd for dw, nd in zip(self.delta_weights, new_deltas)]
 
     def remember_epoch(self, epoch_size=1):
-        self.weights = [w + dw/epoch_size for w, dw in zip(self.weights, self.delta_weights)]
+        self.weights = [w + dw / epoch_size for w, dw in zip(self.weights, self.delta_weights)]
         self.delta_weights = np.zeros(len(self.weights))
 
 
@@ -92,7 +92,8 @@ class Network:
         if self.levels is not None:
             for w in range(width):
                 level.append(Neuron(inputs=self.levels[-1]))
-            self.levels[-1].outputs = level
+            for n in self.levels[-1]:
+                n.outputs = level
         else:
             self.levels = []
             for w in range(width):
@@ -100,69 +101,105 @@ class Network:
 
         self.levels.append(level)
 
-    def output(self):
+    def output(self, new_input=None):
+        if new_input is not None:
+            self.set_input(new_input)
         return [n.output() for n in self.levels[-1]]
 
-    def teach_row(self, input_row, expected):
+    def top_down_iter(self):
+        for level in reversed(self.levels):
+            for neuron in level:
+                yield neuron
+
+    def set_input(self, input_row):
         for n in self.levels[0]:
             n.inputs = input_row
-        for level in self.levels:
-            for neuron in level:
-                neuron.teach(expected)
+
+    def teach_row(self, input_row, expected):
+        self.set_input(input_row)
+        for n in self.top_down_iter():
+            n.teach(expected)
+
+    def teach_epoch(self, teaching_epoch):
+        for input_row, expected in teaching_epoch:
+            self.teach_row(input_row, expected)
+        for n in self.top_down_iter():
+            n.remember_epoch(epoch_size=len(teaching_epoch))
+
+    def teach(self, teaching_set, batch_size, max_iter=1000):
+        for i in range(max_iter):
+            rand.shuffle(teaching_set)
+            for batch in [teaching_set[i:i + batch_size] for i in range(0, len(teaching_set), batch_size)]:
+                self.teach_epoch(batch)
 
 
-hidden = [Neuron(inputs=[0, 0]),
-          Neuron(inputs=[0, 0]),
-          Neuron(inputs=[0, 0])]
+def teaching_script():
+    hidden = [Neuron(inputs=[0, 0]),
+              Neuron(inputs=[0, 0]),
+              Neuron(inputs=[0, 0])]
 
-top = Neuron(inputs=hidden)
-
-for n in hidden:
-    n.outputs = [top]
-
-for i in range(1000):
-
-    for n in hidden:
-        n.inputs = [0, 0]
-
-    print("00 iter " + str(i) + "\t" + str(top.output()))
-
-    top.teach(0)
-    for n in hidden:
-        n.teach(0)
-
-    for n in hidden:
-        n.inputs = [1, 0]
-
-    print("10 iter " + str(i) + "\t" + str(top.output()))
-
-    top.teach(1)
-    for n in hidden:
-        n.teach(1)
+    top = Neuron(inputs=hidden)
 
     for n in hidden:
-        n.inputs = [1, 1]
+        n.outputs = [top]
 
-    print("11 iter " + str(i) + "\t" + str(top.output()))
+    for i in range(1000):
 
-    top.teach(0)
+        for n in hidden:
+            n.inputs = [0, 0]
+
+        print("00 iter " + str(i) + "\t" + str(top.output()))
+
+        top.teach(0)
+        for n in hidden:
+            n.teach(0)
+
+        for n in hidden:
+            n.inputs = [1, 0]
+
+        print("10 iter " + str(i) + "\t" + str(top.output()))
+
+        top.teach(1)
+        for n in hidden:
+            n.teach(1)
+
+        for n in hidden:
+            n.inputs = [1, 1]
+
+        print("11 iter " + str(i) + "\t" + str(top.output()))
+
+        top.teach(0)
+        for n in hidden:
+            n.teach(0)
+
+        for n in hidden:
+            n.inputs = [0, 1]
+
+        print("01 iter " + str(i) + "\t" + str(top.output()))
+
+        top.teach(1)
+        for n in hidden:
+            n.teach(1)
+
+        top.remember_epoch(4)
+        for n in hidden:
+            n.remember_epoch(4)
+
     for n in hidden:
-        n.teach(0)
+        print(n.weights)
+    print()
+    print(top.weights)
 
-    for n in hidden:
-        n.inputs = [0, 1]
 
-    print("01 iter " + str(i) + "\t" + str(top.output()))
+def teaching_test_xor():
+    t_set = [([0, 0], 0), ([1, 1], 0), ([0, 1], 1), ([1, 0], 1)]
+    net = Network(2, [3, 1], 2)
+    net.teach(t_set, 4, 1000)
+    # print(net.output([0, 0]))
+    # print(net.output([1, 1]))
+    # print(net.output([0, 1]))
+    # print(net.output([1, 0]))
+    return (net.output([0, 0])[0] + net.output([1, 1])[0] < 0.2) and (
+        net.output([1, 0])[0] + net.output([0, 1])[0] > 1.85)
 
-    top.teach(1)
-    for n in hidden:
-        n.teach(1)
 
-    top.remember_epoch(4)
-    for n in hidden:
-        n.remember_epoch(4)
-
-for n in hidden:
-    print(n.weights)
-print()
-print(top.weights)
